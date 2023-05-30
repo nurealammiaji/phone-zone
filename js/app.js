10070e5904440d2059f590a86acc75e6bad7434e
nurealammiaji <nurealam.miaji@gmail.com>
2023-05-30 22:14:07 +0600

    dynamic value added to the card

--- js/app.js
+++ js/app.js
@@ -14,10 +14,12 @@ let displayPhones = (phones) => {
         phoneDiv.classList.add("Col");
         phoneDiv.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${phone.image}" class="container border border-round card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <h6>Brand: ${phone.brand}</h6>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button></button>
             </div>
         </div>
         `;
