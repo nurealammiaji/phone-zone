99141be5bb4ba193a04a4e9406ae1f9a0a1e0496
nurealammiaji <nurealam.miaji@gmail.com>
2023-05-30 21:48:23 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -7,8 +7,21 @@ let loadPhones = async() => {
 
 let displayPhones = (phones) => {
     console.log(phones.data);
+    let phonesContainer = document.getElementById("phones-container");
     phones.data.forEach(phone => {
         console.log(phone);
+        let phoneDiv = document.createElement("div");
+        phoneDiv.classList.add("Col");
+        phoneDiv.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
     })
 }
 
