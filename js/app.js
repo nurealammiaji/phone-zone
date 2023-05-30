8c28602bab537defce2c0a0faf7134035314b3fe
nurealammiaji <nurealam.miaji@gmail.com>
2023-05-30 23:14:58 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -11,15 +11,32 @@ let displayPhones = (phones) => {
     phones.data.forEach(phone => {
         console.log(phone);
         let phoneDiv = document.createElement("div");
-        phoneDiv.classList.add("Col");
+        phoneDiv.classList.add("col");
         phoneDiv.innerHTML = `
-        <div class="card">
-            <img src="${phone.image}" class="container border border-round card-img-top" alt="...">
+        <div class="card col">
+            <img src="${phone.image}"  class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <h6>Brand: ${phone.brand}</h6>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button></button>
+                <p class="card-text"></p>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
+            </div>
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">${phone.phone_name}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        Brand:
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         `;
