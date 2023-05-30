f633d94699636e43bb90218534ed5cf298b563b4
nurealammiaji <nurealam.miaji@gmail.com>
2023-05-30 20:32:01 +0600

    data loading and displaying function added

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@ let loadPhones = async() => {
     let url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
     let res = await fetch(url);
     let data = await res.json();
-    console.log(data);
+    displayPhones(data);
 }
+
+let displayPhones = (phones) => {
+    console.log(phones.data);
+    phones.data.forEach(phone => {
+        console.log(phone);
+    })
+}
+
+loadPhones();
