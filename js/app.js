b3bebcbadfec433ff50c9049a79627119c3ade19
nurealammiaji <nurealam.miaji@gmail.com>
2023-05-31 10:35:36 +0600

    search functionality added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
-let loadPhones = async() => {
-    let url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+let loadPhones = async(value) => {
+    let url = `https://openapi.programming-hero.com/api/phones?search=${value}`;
     let res = await fetch(url);
     let data = await res.json();
     displayPhones(data);
 }
 
 let displayPhones = (phones) => {
-    console.log(phones.data);
     let phonesContainer = document.getElementById("phones-container");
+    phonesContainer.innerText = "";
     phones.data.forEach(phone => {
         console.log(phone);
+        let phoneName = phone.phone_name;
         let phoneDiv = document.createElement("div");
         phoneDiv.classList.add("col");
         phoneDiv.innerHTML = `
@@ -19,24 +20,6 @@ let displayPhones = (phones) => {
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <h6>Brand: ${phone.brand}</h6>
                 <p class="card-text"></p>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
-            </div>
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="staticBackdropLabel">${phone.phone_name}</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                        Brand:
-                        </div>
-                        <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         `;
@@ -44,4 +27,9 @@ let displayPhones = (phones) => {
     })
 }
 
-loadPhones();
+let searchPhone = () => {
+    let searchField = document.getElementById("search-field").value;
+    loadPhones(searchField);
+}
+
+loadPhones(value = 'iphone');
